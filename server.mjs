ae00ca06befa11e75761435822c07bc0ef4c4400
mortenera11 <85340612+mortenera11@users.noreply.github.com>
2022-07-02 18:38:33 +0200

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,12 +1,12 @@
 import { createServer } from 'http';
 
 const fs = require('fs').promises;
-createServer((req, res) => {
-  res.write('Hello World!');
+createServer( async (req, res) => {
+  res.write( await loadFile() );
   res.end();
 }).listen(process.env.PORT);
 
-async function loadMonoCounter() {
+async function loadFile() {
     const data = await fs.readFile("index.html", "binary");
     return Buffer.from(data);
 }
